99aa9250231aa5feed3948c4a8704714cc29e160
Uzair Ashraf <uzinatorcl@live.com>
2019-12-04 20:53:01 -0800

    first commit

---
+++ playground/Assets/player.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : MonoBehaviour {
+    public Vector3 startPosition;
+	// Use this for initialization
+	void Start () {
+        transform.position = startPosition;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ playground/Assets/playermoverment.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playermoverment : MonoBehaviour {
+    [SerializeField]
+    float speed;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+        transform.Translate(new Vector3(horizontalInput,verticalInput,0) * Time.deltaTime * speed);
+	}
+}
